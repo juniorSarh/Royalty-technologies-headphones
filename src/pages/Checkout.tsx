8508f08ad7a86c2e57c93b7fea12bd00ce933089
juniorSarh <sabelohgumede@gmail.com>
2025-09-15 11:55:43 +0200

    Added pages on src folder

---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Checkout() {
+  return (
+    <div>
+    <h1> 
+      Where payments are made
+      </h1>
+    </div>
+  )
+}
---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Gallery() {
+  return (
+    <div>
+        <h1>
+            Gallery page for headphones
+        </h1>
+      
+    </div>
+  )
+}
